a093b65e5faa94bf5820f601fad36ca61cf3368b
Ivan Baksheev <zpix-dev@list.ru>
2021-02-04 13:20:13 +0700

    2: done

---
+++ 2/main.c
@@ -0,0 +1,19 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+extern char *tzname[];
+int main() {
+    time_t now;
+    struct tm *sp;
+    time(&now);
+    now -= 60 * 60 * (7 + 8);
+    printf("%s", ctime(&now));
+
+    // sp = localtime(&now);
+    // printf("%d/%d/%d %d:%02d %s\n",
+    //      sp->tm_mon + 1, sp->tm_mday,
+    //      sp->tm_year, sp->tm_hour,
+    //      sp->tm_min, tzname[sp->tm_isdst]);
+    exit(0);
+}
