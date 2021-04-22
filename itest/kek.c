58f2acb0670f6bb74cddc5ae8c22b0088a2deb63
Ivan Baksheev <zpix-dev@list.ru>
2021-04-22 23:42:05 +0700

    garbage removed

--- itest/kek.c
+++
@@ -1,9 +0,0 @@
-int kek() {
-    char a[8];
-    printf("kek: %p\na(stack): %p\n", &kek, a);
-    printf("%p %p %p %p %p %p %p %p %p %p %p\n");
-    scanf("%s", a);
-    printf("%p %p %p %p %p %p %p %p %p %p %p\n");
-
-}
-int main() {kek();}
